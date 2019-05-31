9e2148569d787772165f1afe74eb4b6ae0144229
Katongole Allan <kapsonkatongole@gmail.com>
2019-05-31 08:40:50 +0300

    Fix certifi

--- uni_chain_api_simu/uni_chain_api_simu/settings.py
+++ uni_chain_api_simu/uni_chain_api_simu/settings.py
@@ -24,6 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_bc8!_b=e12=^#np)!8y(y8=&@jk3xgyu39%-k^)lbcgr&s-d^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+#DEBUG = True
 DEBUG = False
 
 #ALLOWED_HOSTS = ['localhost', '127.0.0.1']
